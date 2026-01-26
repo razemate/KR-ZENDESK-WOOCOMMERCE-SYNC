f25d046172613a9b08c648985e6e5b71c7d1eb38
razemate <razemate@gmail.com>
2026-01-27 02:57:32 +0800

    Add copy email icon to No Data view

--- assets/main.js
+++ assets/main.js
@@ -54,7 +54,7 @@ function renderError(title, detailsObj) {
 
 function renderNoData(email) {
   renderBox("KR Zendesk Woo Sync",
-    (email ? '<div class="row muted">Requester: ' + esc(email) + '</div>' : '') +
+    (email ? '<div class="row muted">Requester: ' + esc(email) + ' <span class="copy-icon" title="Copy Email" onclick="copyToClipboard(\'' + esc(email) + '\')"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg></span></div>' : '') +
     '<div class="row">No Woo subscription data found.</div>' +
     '<div class="row"><button id="refreshBtn">Refresh</button></div>'
   );
